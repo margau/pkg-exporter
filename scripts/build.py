2269b1d775223fcdf2ec38bcc04c15a1ab506207
Marvin Gaube <dev@marvingaube.de>
2021-06-26 21:14:50 +0200

    Feature: Add new pyinstaller builds (#1)
    * Add pyinstaller builds
    
    * Pyinstaller Workflow
    
    * Build Using python 3.7
    
    * Python3 Apt
    
    * workflow apt
    
    * Build: Fix Pip
    
    * Fix pkg exporter name
    
    * Build Workflow

---
+++ scripts/build.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import os
+import subprocess
+
+cwd = os.path.join(os.path.dirname(__file__), "..")
+cwd = os.path.abspath(cwd)
+
+subprocess.run(["pyinstaller", "--onefile", "-n", "pkg-exporter", "texfile.py"], cwd=cwd)
